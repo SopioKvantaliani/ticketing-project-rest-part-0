8585738c7cc4d899ab9b539f8a2b8f4cf9168ff6
sopiokvantaliani <skvantaliani@gmail.com>
2023-06-09 14:02:59 -0400

    Creating endpoints in userService

---
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -0,0 +1,60 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+import com.cydeo.service.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.w3c.dom.stylesheets.LinkStyle;
+
+import java.util.List;
+
+@RestController //if we have only @Controller annotation we need to return view
+@RequestMapping ("/api/v1/user")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public ResponseEntity <ResponseWrapper> getUsers (){
+        List <UserDTO> userDTOList = userService.listAllUsers();
+        return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieves", userDTOList, HttpStatus.OK));
+        /*
+        When we want to see status code in the body, we add in the contractor HttpStatus.Ok
+         */
+    }
+
+    @GetMapping("/{username}")
+    public ResponseEntity <ResponseWrapper> getUserByUserName (@PathVariable("username") String username){ //PathVariable should match the endPoint and it will be assigned to String username;
+        UserDTO user = userService.findByUserName(username);
+        return ResponseEntity.ok(new ResponseWrapper("User is successfully retrieved", user, HttpStatus.OK ));
+
+    }
+
+    @PostMapping
+    public ResponseEntity <ResponseWrapper> createUser (@RequestBody UserDTO user){ //UserDTO comes from API and to capture we need to use @RequestBody annotation
+        userService.save(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body
+                (new ResponseWrapper("User is successfully created", user, HttpStatus.CREATED ));
+
+    }
+
+    @PutMapping
+    public ResponseEntity <ResponseWrapper> updateUser (@RequestBody UserDTO user){
+        userService.update(user);
+        return ResponseEntity.ok
+                (new ResponseWrapper("User is successfully updated", user, HttpStatus.OK ));
+    }
+    @DeleteMapping("/{username}")
+    public ResponseEntity <ResponseWrapper> deleteUser (@PathVariable("username") String userName){
+        userService.delete(userName);
+        return ResponseEntity.ok
+                (new ResponseWrapper("User is successfully deleted", userName, HttpStatus.OK ));
+    }
+}
