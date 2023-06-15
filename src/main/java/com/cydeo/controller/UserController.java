e778402ac6e12632fe6619be6fd8156ee597707b
sopiokvantaliani <skvantaliani@gmail.com>
2023-06-14 20:36:34 -0400

    Swagger documentation and connection with KeyCloak

--- src/main/java/com/cydeo/controller/UserController.java
+++ src/main/java/com/cydeo/controller/UserController.java
@@ -3,6 +3,8 @@ package com.cydeo.controller;
 import com.cydeo.dto.ResponseWrapper;
 import com.cydeo.dto.UserDTO;
 import com.cydeo.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,6 +14,7 @@ import java.util.List;
 
 @RestController //if we have only @Controller annotation we need to return view
 @RequestMapping ("/api/v1/user")
+@Tag(name = "UserController", description = "User API")
 public class UserController {
 
     private final UserService userService;
@@ -22,9 +25,10 @@ public class UserController {
 
     @GetMapping
     @RolesAllowed({"Manager", "Admin"}) // Totally depends on the business logic.
+   @Operation(summary = "Get users")
     public ResponseEntity <ResponseWrapper> getUsers (){
         List <UserDTO> userDTOList = userService.listAllUsers();
-        return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieves", userDTOList, HttpStatus.OK));
+        return ResponseEntity.ok(new ResponseWrapper("Users are successfully retrieved", userDTOList, HttpStatus.OK));
         /*
         When we want to see status code in the body, we add in the contractor HttpStatus.Ok
          */
@@ -32,6 +36,7 @@ public class UserController {
 
     @GetMapping("/{username}")
     @RolesAllowed("Admin")
+    @Operation(summary = "Get user by username")
     public ResponseEntity <ResponseWrapper> getUserByUserName (@PathVariable("username") String username){ //PathVariable should match the endPoint and it will be assigned to String username;
         UserDTO user = userService.findByUserName(username);
         return ResponseEntity.ok(new ResponseWrapper("User is successfully retrieved", user, HttpStatus.OK ));
@@ -40,6 +45,7 @@ public class UserController {
 
     @PostMapping
     @RolesAllowed("Admin")
+    @Operation(summary = "Create User")
     public ResponseEntity <ResponseWrapper> createUser (@RequestBody UserDTO user){ //UserDTO comes from API and to capture we need to use @RequestBody annotation
         userService.save(user);
         return ResponseEntity.status(HttpStatus.CREATED).body
@@ -49,6 +55,7 @@ public class UserController {
 
     @PutMapping
     @RolesAllowed("Admin")
+    @Operation(summary = "Update User")
     public ResponseEntity <ResponseWrapper> updateUser (@RequestBody UserDTO user){
         userService.update(user);
         return ResponseEntity.ok
@@ -56,6 +63,7 @@ public class UserController {
     }
     @DeleteMapping("/{username}")
     @RolesAllowed("Admin")
+    @Operation(summary = "delete user")
     public ResponseEntity <ResponseWrapper> deleteUser (@PathVariable("username") String userName){
         userService.delete(userName);
         return ResponseEntity.ok
